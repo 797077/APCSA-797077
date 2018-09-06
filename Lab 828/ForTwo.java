7cf2304e58d7ed8b48ad95c244cacbd257c34b02
797077 <42552641+797077@users.noreply.github.com>
2018-09-06 10:36:11 -0700

    Add files via upload

---
+++ Lab 828/ForTwo.java
@@ -0,0 +1,18 @@
+//© A+ Computer Science
+// www.apluscompsci.com
+
+//for loop example 2
+
+import static java.lang.System.*;
+
+public class ForTwo
+{
+	public static void main(String args[])
+	{
+		for(int run=1; run<11; run+=3)  //change the stop and increment
+		{
+		   out.println("loop");
+		   out.println(run);
+		}
+	}
+}
