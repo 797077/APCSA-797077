23fdd2072d6c7937b2c68a95136878756a7ba983
797077 <42552641+797077@users.noreply.github.com>
2018-09-06 10:33:56 -0700

    Add files via upload

---
+++ Lab 820/StudentRunner.java
@@ -0,0 +1,20 @@
+
+/**
+ * Lab 816 First Class
+ *
+ * @Grace Jau
+ * @version 820
+ */
+public class StudentRunner
+{
+    // instance variables - replace the example below with your own
+    public static void main(){
+        Student s1 = new Student();
+        System.out.println(s1.getStudNumber());
+        System.out.println(s1.getStuActive());
+        System.out.println(s1.getStuGPA());
+        System.out.println(s1.getStuName());
+    }
+
+
+}
