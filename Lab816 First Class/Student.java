014ffbc34cdf7ed14367998efdc6dc1a6803343e
797077 <42552641+797077@users.noreply.github.com>
2018-09-06 10:36:54 -0700

    Add files via upload

---
+++ Lab816 First Class/Student.java
@@ -0,0 +1,38 @@
+
+/**
+ * Lab 816 First Class
+ *
+ * @Grace Jau
+ * @version 816
+ */
+public class Student
+{
+    /**
+     * Instance variables of class Student
+     */
+    private int studNumber;
+
+    /**
+     * Constructor for objects of class Student
+     */
+    public Student()
+    {
+        // initialise instance variables
+        studNumber = 0;
+    }
+
+    /**
+     * Student Methods
+     *
+     */
+    public void getStudNumber(int sn)
+    {
+        // put your code here
+        studNumber = sn;
+    }
+    public int getStudNumber()
+    {
+        // put your code here
+        return studNumber;
+    }
+}
---
+++ Lab816 First Class/StudentRunner.java
@@ -0,0 +1,17 @@
+
+/**
+ * Lab 816 First Class
+ *
+ * @Grace Jau
+ * @version 816
+ */
+public class StudentRunner
+{
+    // instance variables - replace the example below with your own
+    public static void main(){
+        Student s1 = new Student();
+        System.out.println(s1.getStudNumber());
+    }
+
+
+}
