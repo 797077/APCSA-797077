9da4ad120fc4112166f6df8617452b41e11408be
797077 <42552641+797077@users.noreply.github.com>
2018-10-27 18:15:52 -0700

    Update StudentRunner.java

--- Projects/ArrayList Project/StudentRunner.java
+++ Projects/ArrayList Project/StudentRunner.java
@@ -1,30 +1,47 @@
-
+import java.util.Scanner;
 /**
  * ArrayList Project
  *
  * @Grace Jau
  * @version 1025
  */
-public class StudentRunner
+public class StudListRunner
 {
-    // instance variables - replace the example below with your own
     public static void main(){
-        Student s1 = new Student();
-        System.out.println(s1.getStudNumber());
-        System.out.println(s1.getStuActive());
-        System.out.println(s1.getStuGPA());
-        System.out.println(s1.getStuName());
-        System.out.println();
-        
-        String[] studNames = {"name1", "name2", "name3", "name4", "name5"};
-        int[] studNums = {1, 2, 3, 4, 5};
-        for(int i = 0; i<5; i++){
-            Student s = new Student();
-            s.setStudNumber(studNums[i]);
-            s.setStuName(studNames[i]);
-            System.out.println(s.getStuName()+"   "+s.getStudNumber());
+        StudList sl = new StudList();
+        Scanner in = new Scanner (System.in);
+        String statement = "";
+        while (!statement.equals("7")){
+            System.out.println("Main Menu: Please enter the number of the option you would like to choose.");
+            System.out.println("1. Add a student");
+            System.out.println("2. Delete a student");
+            System.out.println("3. Edit the list of students");
+            System.out.println("4. Clear the list of students");
+            System.out.println("5. Print all of the students' information");
+            System.out.println("6. Print a student's information");
+            System.out.println("7. Exit");
+            System.out.println();
+            statement = in.nextLine();
+            if (statement.equals("1")){
+                System.out.println("Please enter the new student's name.");
+                String name = in.nextLine();
+                sl.addStudent(name);
+                System.out.println("The student has been added.");
+            }else if (statement.equals("2")){
+                sl.deleteStudent();
+            }else if (statement.equals("3")){
+                sl.editStudentList();
+            }else if (statement.equals("4")){
+                sl.clearList();
+            }else if (statement.equals("5")){
+                sl.printAll();
+            }else if (statement.equals("6")){
+                sl.printStudent();
+            }else if (!statement.equals("7")){
+                System.out.println("That was not a valid command. Please try again.");
+            }
+            System.out.println();
         }
+        System.out.println("Exited the program.");
     }
-
-
 }
