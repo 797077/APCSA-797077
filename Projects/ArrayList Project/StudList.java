fb5c669adbb98ea287e4e8271d478e7e4953c89f
797077 <42552641+797077@users.noreply.github.com>
2018-10-27 18:14:41 -0700

    Update StudList.java

--- Projects/ArrayList Project/StudList.java
+++ Projects/ArrayList Project/StudList.java
@@ -1,30 +1,94 @@
-import.java.lang.ArrayList;
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  * ArrayList Project
  *
  * @Grace Jau
  * @version 1025
  */
-public class StudList
-{
+public class StudList{
     ArrayList<Student>studList;
     /**
      * Constructor for objects of class StudList
      */
-    public StudList()
-    {
-        // initialise instance variables
-        
+    public StudList(){
+        studList = new ArrayList<Student>();
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * comment
      */
-    public void sampleMethod()
-    {
-        
+    public void parseUserInput(String name, Student student){
+        String fn = ""; String mn = ""; String ln = "";
+        int i = name.indexOf(" ");
+        int j = name.indexOf(" ", i+1);
+        if (name.indexOf(",") == -1){
+            fn = name.substring(0, i);
+            j = name.indexOf(" ", i+1);
+            mn = name.substring(i+1, j);
+            ln = name.substring(j+1);
+        }else{
+            if (j == -1){
+                ln = name.substring(0, i-1);
+                fn = name.substring(i+1);
+            }else{
+                ln = name.substring(0, i-1);
+                j = name.indexOf(" ", i+1);
+                fn = name.substring(i+1, j);
+                mn = name.substring(j+1);
+            }
+        }
+        student.setFirstName(fn);
+        student.setMiddleName(mn);
+        student.setLastName(ln);
+    }
+
+    /**
+     * comment
+     */
+    public void addStudent(String name){
+        studList.add(new Student());
+        parseUserInput(name, studList.get(studList.size()-1));
+    }
+
+    /**
+     * comment
+     */
+    public void deleteStudent(){
+
+    }
+
+    /**
+     * comment
+     */
+    public void editStudentList(){
+
+    }
+
+    /**
+     * comment
+     */
+    public void clearList(){
+        studList.clear();
+    }
+
+    /**
+     * comment
+     */
+    public void printAll(){
+        if (studList.size()<=0){
+            System.out.println("The student list is empty.");
+        }else{
+            for (int i = 0; i < studList.size(); i++){
+                System.out.println(studList.get(i).getFullName()+"   "+studList.get(i).getStuGPA()+"   "+studList.get(i).getStuNumber());
+            }
+        }
+    }
+
+    /**
+     * comment
+     */
+    public void printStudent(){
+
     }
 }
